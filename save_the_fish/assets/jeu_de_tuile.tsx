ae893ac928f90a47abb514f623697c072311deec
LambertPierre <74670133+LambertPierre@users.noreply.github.com>
2021-03-28 16:55:12 +0200

    Projet Prog/Info2D

---
+++ save_the_fish/assets/jeu_de_tuile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="jeu_de_tuile" tilewidth="20" tileheight="20" tilecount="10368" columns="96">
+ <image source="jeu_de_tuile.png" width="1920" height="2160"/>
+</tileset>
